05b5ac89cab1f2ea94484736f2ffc2d147515ed2
jayant <jayantarsode234@gmail.com>
2026-09-23 00:55:23 +0530

    Back to default prompt_toolkit

--- app/main.py
+++ app/main.py
@@ -3,28 +3,28 @@ from app.handlers.command_handler import _execute_command
 
 def main():
     # 1. (Recommended) This is advance command completion and this is cross platform.
-    # from prompt_toolkit import PromptSession
-    # from app.handlers.completion_handler import ShellCompleter
-    # from prompt_toolkit.cursor_shapes import CursorShape
+    from prompt_toolkit import PromptSession
+    from app.handlers.completion_handler import ShellCompleter
+    from prompt_toolkit.cursor_shapes import CursorShape
 
-    # session = PromptSession(
-    #     completer=ShellCompleter(),
-    #     cursor=CursorShape.BLOCK,
-    #     complete_while_typing=False,
-    # )
+    session = PromptSession(
+        completer=ShellCompleter(),
+        cursor=CursorShape.BLOCK,
+        complete_while_typing=False,
+    )
 
     # 2. For linux only, swap the block above for readline based completion.
-    import readline
-    from app.handlers.completion_handler import __auto_shell_completion
+    # import readline
+    # from app.handlers.completion_handler import __auto_shell_completion
 
-    readline.set_completer(__auto_shell_completion)
-    readline.set_completer_delims(" \t\n")
-    readline.parse_and_bind("tab: complete")
+    # readline.set_completer(__auto_shell_completion)
+    # readline.set_completer_delims(" \t\n")
+    # readline.parse_and_bind("tab: complete")
 
     while True:
         try:
-            # shell_input = session.prompt("$ ")
-            shell_input = input("$ ")
+            shell_input = session.prompt("$ ")
+            # shell_input = input("$ ")
             evaluated = _execute_command(shell_input)
             if evaluated != "exit":
                 if evaluated != None:
